3206af0ddd0cdb8717fd410a21b4e7e2cca5abd8
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2019-12-19 05:02:23 -0800

    Add GuessMyNumber While Looop

--- Control-Flow.py
+++ Control-Flow.py
@@ -4,3 +4,13 @@ Date: 12.16.19
 Program: Guess My Number
 """
 
+my_number = 10
+
+# Ask the user to guess
+guess = int(input("\nEnter a guess: "))
+
+# Keep asking until the guess becomes equal to the secret number
+while guess != my_number:
+    print ("Your an idiot\n")
+    guess = int(input("Try Again Stupid: "))
+print ("\nLet me poop in your mouth")
