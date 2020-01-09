3cb3cbf129b2485cbfb7d86a31643eaccd5bf482
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2020-01-09 04:55:15 -0800

    Add round & updated format

--- Control-Flow.py
+++ Control-Flow.py
@@ -1,14 +1,14 @@
 """
-Programmer: Commander Kofi
+Programmer: Commander Kofi Ladd-Oliver
 Date: 12.16.19
 Program: Guess My Number
 """
+total = 0
+how_many_tests = int(input("How many tests would you like to average: "))
+print("")
 
-sum = 10
-how_many_numbers = int(input("How many numbers you want to sum: "))
-
-for i in range(how_many_numbers):
-    enter_a_number = int(input("\nEnter a number: "))
-    sum = sum + enter_a_number
-
-print("\nSum of your numbers is : " + str(sum))
+for i in range(how_many_tests):
+    enter_a_score = int(input("Enter a score: "))
+    total = total + enter_a_score
+    avg = total / how_many_tests
+print("\nAverage: " + str(round(avg, 2)))
