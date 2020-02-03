ba2af90af71d9638c49d8637d245a99645fe7168
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2020-02-03 05:35:05 -0800

    Add Greeting

---
+++ Control-Flow.py
@@ -0,0 +1,14 @@
+# Date: 2.3.20
+# Greeting Function
+
+name = input("\nWhat is your name: ")
+
+# create Funtions here
+def greeting():
+    print("Hello " + name)
+    print(name + " your a retarded ")
+
+# call function here
+greeting()
+
+
