0a7e7369dd911ee9deb6f2e809a32bff316344a3
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2020-02-04 04:48:42 -0800

    Functions & Variables = printSomething()

--- Control-Flow.py
+++ Control-Flow.py
@@ -1,14 +1,23 @@
 # Date: 2.3.20
 # Greeting Function
+# Global Varibles here
 
 name = input("\nWhat is your name: ")
+
+x = 15
+
 
 # create Funtions here
 def greeting():
     print("Hello " + name)
-    print(name + " your a retarded ")
+    print(name + " thats a cool name")
+    print(x)
 
+def printSomething():
+    x = 3
+    print(x)
 # call function here
+
 greeting()
-
-
+printSomething()
+print(x)
