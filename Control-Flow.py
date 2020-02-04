e77cd03eef2d562a2c8810a03f78ea0895d64865
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2020-02-04 05:03:33 -0800

    Add Function & Parameter

--- Control-Flow.py
+++ Control-Flow.py
@@ -1,23 +1,33 @@
+
 # Date: 2.3.20
-# Greeting Function
+# Programmer; Kofi Ladd-Oliver
 # Global Varibles here
 
-name = input("\nWhat is your name: ")
+# name = input("\nWhat is your name: ")
 
 x = 15
 
 
-# create Funtions here
+# create Functions here....
+
+# Greeting Function
 def greeting():
     print("Hello " + name)
-    print(name + " thats a cool name")
-    print(x)
+    print(name + " that's a cool name")
 
+    # Functions & Local Variable x
 def printSomething():
     x = 3
     print(x)
-# call function here
 
-greeting()
-printSomething()
-print(x)
+# Functions & Parameters
+def printNumber(age): # Function name = printNumber with a Parameter of age
+    print(age)
+# call function here...
+
+# greeting()
+# printSomething()
+# print(x)
+printNumber(28)
+printNumber(38)
+
